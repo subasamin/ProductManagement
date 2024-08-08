d3cf721c2ec3cb8e360ea0926a9ca16ad52fd4fd
admin <admin@Bfl.local>
2024-08-08 09:34:18 +0700

    add

--- src/ss16/Demo4/book/Main.java
+++ src/ss16/Demo4/book/Main.java
@@ -8,26 +8,46 @@ public class Main {
         BookDemo book = new ProgrammingBook("cntt",1,2.0,"Duy","Java","sach");
         BookDemo book1 = new ProgrammingBook("cntt",2,2.0,"Duy","Java","sach");
         BookDemo book2 = new ProgrammingBook("cntt",3,2.0,"Duy","Java","sach");
-        BookDemo book3 = new FictionBook("sao hoa",4,3.0,"Duy","vien tuong");
+        BookDemo book3 = new FictionBook("sao hoa",4,3.0,"Duy","vien tuong1");
         BookDemo book4 = new FictionBook("trai dat",5,3.0,"Duy","vien tuong");
-        BookDemo book5 = new FictionBook("sao kim",6,3.0,"Duy","vien tuong");
+        BookDemo book5 = new FictionBook("sao kim",6,3.0,"Duy","vien tuong1");
         List<BookDemo> books = new ArrayList<>();
+        books.add(book);
+        books.add(book1);
+        books.add(book2);
+        books.add(book3);
+        books.add(book4);
+        books.add(book5);
         int i;
         double price = 0;
         for (i = 0; i < books.size(); i++){
             price += books.get(i).getPrice();
         }
         System.out.println("Gia cua 6 quan sach la: "+price);
-        int index=0;
+        int count=0;
         String word;
         for (i = 0; i < books.size(); i++){
             if (books.get(i)instanceof ProgrammingBook){
                 word = ((ProgrammingBook) books.get(i)).getLanguage();
                 if (word.equals("Java")){
-                    index++;
+                    count++;
                 }
             }
         }
-        System.out.println("So quan sach co language la Java la" +index);
+        System.out.println("So quan sach co language la Java la" +count);
+        int count1=0;
+        int count2=0;
+        for (i = 0; i < books.size(); i++){
+            if (books.get(i)instanceof FictionBook){
+                ((FictionBook) books.get(i)).getCategory().equals("vientuong1");
+                count1++;
+                if(100 > (books.get(i).getPrice())){
+                    count2++;
+                }
+            }
+        }
+        System.out.println("So quan sach co category vien tuong 1 la: "+count1);
+        System.out.println("So sach co gia nho hon 100 la"+count2);
+
     }
 }
